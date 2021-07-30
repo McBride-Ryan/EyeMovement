d0baa7c46b0416207e475193d221dcdccbe3f78c
Ryan McBride <63865386+McBride-Ryan@users.noreply.github.com>
2021-07-30 17:11:08 -0400

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,8 +1,12 @@
+// Get the element with the class name 'ball' in HTML
 const balls = document.getElementsByClassName('ball');
+
+// On the mouse move pass in an event to listen where the mouse moves within the screen window
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
+// Now we have a hold of our variable, we can loop through and update the style to transform our image
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
